e610f1cae0ec750d755357eb66c933b02d1ab5ca
Braden <braden@bradenn.com>
2022-11-13 10:08:48 -0800

    Minor UI fix

--- client/src/views/terminal/Home.vue
+++ client/src/views/terminal/Home.vue
@@ -6,25 +6,12 @@ import router from "@/router";
 import App from "@/components/App.vue";
 import Macros from "@/components/widgets/Macros.vue";
 import type {Attribute, Entity, Remote} from "@/types";
-import Error from "@/components/Error.vue";
-import Camera from "@/components/widgets/Camera.vue";
 import type {Notify} from "@/notifications";
+import Weather from '@/components/widgets/Weather.vue';
+import Calendar from '@/components/widgets/Calendar.vue';
 
 const Spotify = defineAsyncComponent({
     loader: () => import('@/components/widgets/Spotify.vue'),
-    errorComponent: Error,
-    timeout: 250
-})
-const Calendar = defineAsyncComponent({
-    loader: () => import('@/components/widgets/Calendar.vue'),
-    errorComponent: Error,
-    timeout: 250
-})
-
-const Weather = defineAsyncComponent({
-    loader: () => import('@/components/widgets/Weather.vue'),
-    errorComponent: Error,
-    timeout: 250
 })
 
 
@@ -108,19 +95,19 @@ function getRoutes() {
     <div :class="``" class="h-100 w-100">
         <div class="home-grid w-100 h-100">
 
-            <div style="grid-column: 1/span 4; grid-row: 1/span 4;">
+            <div style="grid-column: 1/span 3; grid-row: 1/span 4;">
                 <Macros></Macros>
             </div>
 
             <div class="d-flex justify-content-center flex-column align-items-center gap-1"
                  style=" grid-column: 6/span 5; grid-row: 1/span 6;">
                 <Calendar></Calendar>
-                <Camera></Camera>
+                <!--                <Camera></Camera>-->
             </div>
 
-            <div
-                    style="grid-column: 1/span 11; grid-row: 10 / span 4; "
-                    @click="() => {notify.success('Your mom', 'After a day\'s work, I fucked your mom.')}">
+            <div v-if="false"
+                 style="grid-column: 1/span 11; grid-row: 10 / span 4; "
+                 @click="() => {notify.success('Your mom', 'After a day\'s work, I fucked your mom.')}">
                 <div class="label-c1 label-w600 label-o4 px-1 mb-1"
                      style="font-family: 'SF Pro Display', sans-serif">
                     <span class="label-o2">􀎪</span>
