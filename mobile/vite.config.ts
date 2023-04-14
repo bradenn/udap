9fbf10707155c83c6bfabf9322fd21c55ea3c59a
Braden <braden@bradenn.com>
2023-04-14 14:22:03 -0700

    Modified the authorization system in the enrollment app. Added icons. Disabled PWA for mobile.

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from 'url'
-import {VitePWA} from "vite-plugin-pwa";
+// import {VitePWA} from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
@@ -9,37 +9,43 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    build: {
-        sourcemap: process.env.SOURCE_MAP === 'true',
-    },
+    // build: {
+    //     sourcemap: process.env.SOURCE_MAP === 'true',
+    // },
     plugins: [vue(),
-        VitePWA({
-            mode: 'development',
-            manifest: {
-                name: 'UDAP',
-                short_name: 'UDAP',
-                description: 'Universal Device Aggregation Platform Interface',
-                start_url: '/',
-                display: 'standalone',
-                background_color: '#000000',
-                theme_color: '#000000',
-                icons: [
-                    {
-                        src: '/pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                    },
-                    {
-                        src: '/pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                    },
-                ],
-            },
-            workbox: {
-                // Workbox configuration options
-            },
-        }),],
+        // VitePWA({
+        //     mode: 'development',
+        //     registerType: 'autoUpdate',
+        //     manifest: {
+        //         name: 'UDAP',
+        //         short_name: 'UDAP',
+        //         description: 'Universal Device Aggregation Platform Interface',
+        //         start_url: '/',
+        //         display: 'standalone',
+        //         background_color: '#000000',
+        //         theme_color: '#000000',
+        //         icons: [
+        //             {
+        //                 src: '/pwa-192x192.png',
+        //                 sizes: '192x192',
+        //                 type: 'image/png',
+        //             },
+        //             {
+        //                 src: '/pwa-512x512.png',
+        //                 sizes: '512x512',
+        //                 type: 'image/png',
+        //             },
+        //         ],
+        //     },
+        //     devOptions: {
+        //         enabled: true
+        //         /* other options */
+        //     },
+        //     workbox: {
+        //         // Workbox configuration options
+        //     },
+        // }),
+    ],
     server: {
         port: 5045,
     }
