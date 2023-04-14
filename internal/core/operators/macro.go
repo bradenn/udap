cdc681b076abfc90339da2b1971e161d8946229b
Braden <braden@bradenn.com>
2023-04-14 14:06:07 -0700

    removed revertAfter structure from the udap core.

--- internal/core/operators/macro.go
+++ internal/core/operators/macro.go
@@ -3,25 +3,52 @@
 package operators
 
 import (
-	"fmt"
 	"time"
 	"udap/internal/controller"
 	"udap/internal/core/domain"
 	"udap/internal/core/ports"
-	"udap/internal/log"
-	"udap/internal/pulse"
 )
 
 type macroOperator struct {
-	ctrl  *controller.Controller
-	holds map[string]chan bool
+	ctrl     *controller.Controller
+	launcher chan<- domain.Macro
+	holds    map[string]*time.Timer
+	revert   map[string]domain.Macro
 }
 
 func NewMacroOperator(ctrl *controller.Controller) ports.MacroOperator {
-	return &macroOperator{
-		ctrl:  ctrl,
-		holds: map[string]chan bool{},
+	mo := &macroOperator{
+		ctrl:   ctrl,
+		holds:  map[string]*time.Timer{},
+		revert: map[string]domain.Macro{},
 	}
+
+	//go func() {
+	//	select {
+	//	case <-launcher:
+	//
+	//	}
+	//
+	//	for macro, timer := range mo.holds {
+	//		mo.runRevert(macro)
+	//	}
+	//}()
+
+	return mo
+}
+
+func (m *macroOperator) runRevert(macro domain.Macro) error {
+	zone, err := m.ctrl.Zones.FindById(macro.ZoneId)
+	if err != nil {
+		return err
+	}
+	for _, entity := range zone.Entities {
+		err = m.ctrl.Attributes.Request(entity.Id, macro.Type, macro.Value)
+		if err != nil {
+			continue
+		}
+	}
+	return nil
 }
 
 func (m *macroOperator) Run(macro domain.Macro) error {
@@ -35,6 +62,12 @@ func (m *macroOperator) Run(macro domain.Macro) error {
 			continue
 		}
 	}
+	//if m.holds[macro.Id] != nil {
+	//	m.holds[macro.Id].Stop()
+	//	delete(m.holds, macro.Id)
+	//	delete(m.revert, macro.Id)
+	//}
+
 	return nil
 }
 
@@ -63,31 +96,26 @@ func (m *macroOperator) RunAndRevert(macro domain.Macro, baseline domain.Macro, 
 		return nil
 	}
 
-	go func() {
-		begin := time.Now()
-		tag := fmt.Sprintf("macro.%s.%s", macro.Id, begin)
-
-		pulse.Begin(tag)
-		defer pulse.End(tag)
-		<-time.NewTimer(revert * time.Minute).C
-		log.Event("REVERT TIME!!! %s (%s) (%dm)", tag, time.Since(begin), revert)
-		zone, err = m.ctrl.Zones.FindById(baseline.ZoneId)
-		if err != nil {
-			return
-		}
-		for _, entity := range zone.Entities {
-			var attr *domain.Attribute
-			attr, err = m.ctrl.Attributes.FindByComposite(entity.Id, macro.Type)
-			if err != nil {
-				continue
-			}
-			err = m.ctrl.Attributes.Request(entity.Id, baseline.Type, values[attr.Id])
-			if err != nil {
-				continue
-			}
-		}
-
-	}()
+	////begin := time.Now()
+	////tag := fmt.Sprintf("macro.%s.%s", macro.Id, begin)
+	//if m.holds[macro.Id] != nil {
+	//	m.holds[macro.Id].Reset(revert * time.Minute)
+	//} else {
+	//	m.holds[macro.Id] = time.NewTimer(revert * time.Minute)
+	//	go func() {
+	//		_ = <-m.holds[macro.Id].C
+	//		if m.holds[macro.Id] != nil {
+	//			log.Event("REVERTING '%s'.", macro.Name)
+	//			err = m.runRevert(m.revert[macro.Id])
+	//			if err != nil {
+	//				return
+	//			}
+	//			delete(m.holds, macro.Id)
+	//			delete(m.revert, macro.Id)
+	//		}
+	//
+	//	}()
+	//}
 
 	return nil
 }
