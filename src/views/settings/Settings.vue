3103f10bb8cf75ef36b994f326a24e06c7e300b6
Braden <braden@bradenn.com>
2023-07-06 16:41:46 -0700

    Fixed a bug where the menus were no longer scrollable

--- src/views/settings/Settings.vue
+++ src/views/settings/Settings.vue
@@ -1,11 +1,14 @@
 <!-- Copyright (c) 2023 Braden Nicholson -->
 
 <script lang="ts" setup>
-import core from "@/core";
-import {computed, reactive} from "vue";
+import core from "udap-ui/core";
+import {PreferencesRemote} from "udap-ui/persistent";
+import {reactive} from "vue";
 import Navbar from "@/components/Navbar.vue";
+import SettingsMenu from "@/views/settings/SettingsMenu.vue";
 
 const router = core.router()
+const preferences = core.preferences() as PreferencesRemote
 
 const state = reactive({
   currentName: ""
@@ -16,18 +19,18 @@ router.afterEach((to, from, failure) => {
   if (!to.name) return
   state.currentName = <string>to.name || "Settings"
 })
-
-let transitionName = computed(() => {
-  if (router.currentRoute.value.path === '/home/settings') {
-    return 'slide-reverse'
-  }
-  return 'slide'
-})
+//
+// let transitionName = computed(() => {
+//   // if (router.currentRoute.value.path === '/home/settings') {
+//   //   return 'slide-up'
+//   // }
+//   // return 'slide-down'
+// })
 
 </script>
 
 <template>
-  <div class=" gap-0 d-flex flex-column" style="height: 100vh">
+  <div class=" gap-0 d-flex flex-column w-100">
     <div class="d-flex">
       <Navbar v-if="router.currentRoute.value.name"
               :back="(router.currentRoute.value.path === '/home/settings')?'/home/dashboard':'/home/settings'"
@@ -35,14 +38,30 @@ let transitionName = computed(() => {
       </Navbar>
     </div>
 
-    <div class="root h-100 w-100" style="overflow-y: scroll">
+    <div v-if="preferences.landscape">
+      <div class="d-flex flex-row gap-1">
+        <div class="col-4">
+          <SettingsMenu></SettingsMenu>
+        </div>
+        <div class="flex-fill w-25">
+          <router-view v-if="router.currentRoute.value.path !== '/home/settings'" v-slot="{ Component }">
+            <transition :name="(router.currentRoute.value.path === '/home/settings')?'slide-reverse':'slide'"
+                        mode="out-in">
+              <component :is="Component"/>
+            </transition>
+          </router-view>
+        </div>
+      </div>
+    </div>
+
+    <div v-else class="root flex-fill mx-1">
+
       <router-view v-slot="{ Component }">
         <transition :name="(router.currentRoute.value.path === '/home/settings')?'slide-reverse':'slide'"
                     mode="out-in">
           <component :is="Component"/>
         </transition>
       </router-view>
-
     </div>
 
   </div>
@@ -52,40 +71,38 @@ let transitionName = computed(() => {
 
 
 .root {
-  height: max-content;
-//margin: auto; //outline: 1px solid white; //display: block; //display: flex; //flex-direction: column; //max-height: 100%; //height: 100%; //overflow-y: scroll;
+
+//overflow-y: auto; //max-height: 50vh; //overflow-y: scroll !important; //height: max-content; //margin: auto; //outline: 1px solid white; //display: block; //display: flex; //flex-direction: column; //max-height: 100%; //height: 100%; //overflow-y: scroll;
 }
 
 @keyframes slide-in-reverse {
   from {
-    transform: translateX(-4%) translateY(-2%) scale(1.025);
-    filter: blur(2px);
-
-    opacity: 0;
+    transform: translateY(-10px);
+    filter: blur(4px);
+    opacity: 0.5;
   }
   to {
-    transform: translateX(0%) translateY(0%) scale(1);
+    transform: translateY(0);
     filter: blur(0px);
-    /**/
     opacity: 1;
   }
 }
 
 @keyframes slide-out-reverse {
   from {
-    transform: translateX(0%) translateY(0%) scale(1);
-    filter: blur(0px);
-    opacity: 1;
+    transform: translateY(-10px);
+    filter: blur(4px);
+    opacity: 0.5;
   }
   to {
-    transform: translateX(4%) translateY(0%) scale(0.7);
-    filter: blur(8px);
-    opacity: 0;
+    transform: translateY(0);
+    filter: blur(0px);
+    opacity: 1;
   }
 }
 
 .slide-reverse-enter-active {
-  animation: slide-in-reverse 70ms linear;
+//animation: slide-in 70ms linear;
 }
 
 .slide-reverse-leave-active {
@@ -94,12 +111,12 @@ let transitionName = computed(() => {
 
 @keyframes slide-in {
   from {
-    transform: translateX(5%);
+    transform: scale(0.98);
     filter: blur(4px);
-    opacity: 0;
+    opacity: 0.5;
   }
   to {
-    transform: translateX(0) scale(1);
+    transform: scale(1);
     filter: blur(0px);
     opacity: 1;
   }
@@ -107,14 +124,10 @@ let transitionName = computed(() => {
 
 @keyframes slide-out {
   from {
-    transform: translateX(0) scale(1);
-    filter: blur(0px);
-    opacity: 1;
+  //transform: translateY(0) scale(0.98); //filter: blur(0px); opacity: 1;
   }
   to {
-    transform: translateX(-5%);
-    filter: blur(4px);
-    opacity: 0;
+  //transform: translateY(10px) scale(0); //filter: blur(1px); opacity: 0;
   }
 }
 
@@ -123,7 +136,7 @@ let transitionName = computed(() => {
 }
 
 .slide-leave-active {
-  animation: slide-out 100ms ease-out;
+  animation: slide-out 75ms ease-out;
 }
 
 /*.slide-enter-to {*/
--- src/views/settings/SettingsMenu.vue
+++ src/views/settings/SettingsMenu.vue
@@ -9,30 +9,37 @@ import Title from "udap-ui/components/Title.vue";
 </script>
 
 <template>
-  <div class="d-flex flex-column gap-3 px-2">
+  <div class="d-flex flex-column gap-3">
 
     <Element>
-      <Title title="App"></Title>
-      <List>
-        <ElementLink icon="􀣋" title="General" to="/home/settings/general"></ElementLink>
-        <ElementLink icon="􀉣" title="Connection" to="/home/settings/connection"></ElementLink>
-        <ElementLink icon="􁇵" title="Configuration" to="/home/settings/config"></ElementLink>
+      <List class="gap-3" scroll-y style="height: 72vh">
+        <div>
+          <Title title="App"></Title>
+          <List>
+            <ElementLink icon="􀣋" title="General" to="/home/settings/general"></ElementLink>
+            <ElementLink icon="􀉣" title="Connection" to="/home/settings/connection"></ElementLink>
+            <ElementLink icon="􁇵" title="Configuration" to="/home/settings/config"></ElementLink>
+          </List>
+        </div>
+        <div>
+          <Title title="Appearance"></Title>
+          <List>
+            <ElementLink icon="􀦳" title="Home" to="/home/settings/home"></ElementLink>
+            <ElementLink icon="􀏅" title="Background" to="/home/settings/background"></ElementLink>
+          </List>
+        </div>
+        <div>
+          <Title title="System"></Title>
+          <List>
+            <ElementLink icon="􁓽" title="Entities" to="/home/settings/entities"></ElementLink>
+            <ElementLink icon="􁚊" title="Modules" to="/home/settings/modules"></ElementLink>
+            <ElementLink icon="􀏭" title="Endpoints" to="/home/settings/endpoints"></ElementLink>
+            <ElementLink icon="􀟼" title="Zones" to="/home/settings/zones"></ElementLink>
+          </List>
+        </div>
       </List>
     </Element>
 
-    <Element>
-      <Title title="Appearance"></Title>
-      <List>
-        <ElementLink icon="􀏅" title="Background" to="/home/settings/background"></ElementLink>
-      </List>
-    </Element>
-    <Element>
-      <Title title="System"></Title>
-      <List>
-        <ElementLink icon="􀟗" title="Entities" to="/home/settings/entities"></ElementLink>
-        <ElementLink icon="􀟗" title="Modules" to="/home/settings/modules"></ElementLink>
-      </List>
-    </Element>
   </div>
 </template>
 
