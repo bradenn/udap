c1b9239644c9902c80eef243bfd0df22ea6dbd2d
Braden <braden@bradenn.com>
2023-07-22 10:20:17 -0700

    Improved the internal pulse system

--- internal/pulse/pulse.go
+++ internal/pulse/pulse.go
@@ -3,7 +3,6 @@
 package pulse
 
 import (
-	"runtime"
 	"sync"
 	"time"
 )
@@ -13,17 +12,19 @@ var Timings Timing
 func init() {
 	Timings = Timing{}
 	Timings.mt = sync.RWMutex{}
+	Timings.historyMutex = sync.RWMutex{}
 	Timings.history = map[string]Proc{}
 	Timings.waiting = map[string]Proc{}
-	Timings.handler = make(chan Proc, 16)
+	Timings.handler = make(chan Proc, 32)
 	go Timings.handle()
 }
 
 type Timing struct {
-	waiting map[string]Proc
-	handler chan Proc
-	mt      sync.RWMutex
-	history map[string]Proc
+	waiting      map[string]Proc
+	handler      chan Proc
+	mt           sync.RWMutex
+	historyMutex sync.RWMutex
+	history      map[string]Proc
 }
 
 type Proc struct {
@@ -39,83 +40,90 @@ type Proc struct {
 	Depth     int       `json:"depth"`
 }
 
+// Timings returns the present timings manifest
 func (h *Timing) Timings() (a map[string]Proc) {
 	a = map[string]Proc{}
 	h.mt.RLock()
 	for i, u := range h.history {
-		a[i] = u
+		if time.Since(u.Stop).Seconds() < 10 {
+			a[i] = u
+		} else {
+			delete(h.history, i)
+		}
 	}
 	h.mt.RUnlock()
 	return
 }
 
+// accept processes a proc request, the proc will either be added to the waiting queue or be resolved
+func (h *Timing) accept(proc Proc) {
+	// Send to waiting queue
+	if !proc.Complete {
+		h.waiting[proc.Pointer] = proc
+		return
+	}
+	// Resolve waiting process
+	resident := h.waiting[proc.Pointer]
+	// Mark the process as complete
+	resident.Complete = true
+	// Compute the delta in nanoseconds
+	resident.Delta = int(time.Since(resident.Start).Nanoseconds())
+	// Lock the mutex and record the record
+	h.mt.Lock()
+	h.history[resident.Pointer] = resident
+	h.mt.Unlock()
+	// Delete the resident from the waiting queue
+	delete(h.waiting, resident.Pointer)
+}
+
+// handle accepts incoming timing requests
 func (h *Timing) handle() {
-
-	for proc := range h.handler {
-		if !proc.Complete {
-			h.waiting[proc.Pointer] = proc
-			continue
+	for {
+		select {
+		// Accept any incoming requests, handle them in a new thread
+		case proc := <-h.handler:
+			go h.accept(proc)
 		}
-
-		resident := h.waiting[proc.Pointer]
-		resident.Stop = time.Now()
-		resident.StopNano = resident.Stop.UnixNano()
-		resident.Complete = true
-		resident.Delta = int(time.Since(resident.Start).Nanoseconds())
-		h.mt.Lock()
-		h.history[resident.Pointer] = resident
-		h.mt.Unlock()
-		delete(h.waiting, resident.Pointer)
 	}
 }
 
-func (h *Timing) begin(ref string, pointer string) {
-	proc := Proc{}
-	proc.Pointer = pointer
-	proc.Start = time.Now()
-	proc.StartNano = proc.Start.UnixNano()
-	proc.Complete = false
-	proc.Name = ref
+// begin starts a process timing request
+func (h *Timing) begin(name string, pointer string) {
+	// Record the start time
+	start := time.Now()
+	// Create the process request
+	proc := Proc{
+		Name:      name,
+		Pointer:   pointer,
+		Start:     start,
+		StartNano: start.UnixNano(),
+		Complete:  false,
+	}
+	// Send the request to the handler mux
 	h.handler <- proc
 }
 
-func (h *Timing) end(ref string, pointer string) {
-	proc := Proc{}
-	proc.Pointer = pointer
-	proc.Name = ref
-	proc.Complete = true
+// end resolves a process timing request
+func (h *Timing) end(name string, pointer string) {
+	// Record the stop time
+	stop := time.Now()
+	// Create the process request
+	proc := Proc{
+		Name:     name,
+		Pointer:  pointer,
+		Complete: true,
+		Stop:     stop,
+		StopNano: stop.UnixNano(),
+	}
+	// Send the request to the handler mux
 	h.handler <- proc
-
-}
-
-func BeginTrack(ref string) {
-	pc, _, _, ok := runtime.Caller(1)
-	if !ok {
-		Timings.begin(ref, ref)
-		return
-	}
-	details := runtime.FuncForPC(pc)
-	if details != nil {
-		Timings.begin(ref, details.Name())
-		return
-	}
 
 }
 
 func Begin(ref string) {
-	// pc, _, _, ok := runtime.Caller(1)
-	// details := runtime.FuncForPC(pc)
 	Timings.begin(ref, ref)
-	// if ok && details != nil {
-	// }
 }
 
 func End(ref string) {
-	// pc, _, _, ok := runtime.Caller(1)
-	// details := runtime.FuncForPC(pc)
 	Timings.end(ref, ref)
-
-	// if ok && details != nil {
-	//
-	// }
 }
